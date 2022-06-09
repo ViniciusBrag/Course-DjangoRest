ef9885df45f1b1ed28645f5f34b1de3af9145df0
Vinicius Braga <Vbragadev@gmail.com>
2022-06-08 22:35:48 -0300

    test view

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Vinicius Braga'
+
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe_view.html')
