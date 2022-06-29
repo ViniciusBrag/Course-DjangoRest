10f6f240f56135048c525505ea138b049ca3a35b
Vinicius Braga <Vbragadev@gmail.com>
2022-06-28 22:25:01 -0300

    add test about views and unittest

--- project/recipes/tests.py
+++ project/recipes/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import resolve, reverse
+from project.recipes import views
 
 
 class RecipeURLsTest(TestCase):
@@ -11,6 +12,28 @@ class RecipeURLsTest(TestCase):
         url = reverse('recipes:category', kwargs={'category_id': 1})
         self.assertEqual(url, '/recipes/category/1/')
 
-    def test_recipe_id_correct(self):
+    def test_recipe_detail_id_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 4})
         self.assertEqual(url, '/recipes/4/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_views_function_is_correct(self):
+        view = resolve(
+            reverse(
+                'recipes:category', kwargs={'category_id': 2}
+            )
+        )
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(
+            reverse(
+                'recipes:recipe', kwargs={'id': 5}
+            )
+        )
+        self.assertIs(view.func, views.recipe)
