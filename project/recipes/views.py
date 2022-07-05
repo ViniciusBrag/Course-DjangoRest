3270a10c6d81798339390a82936a6aefad4efad2
Vinicius Braga <Vbragadev@gmail.com>
2022-07-05 01:02:26 -0300

    add tests and fixture on project

--- project/recipes/views.py
+++ project/recipes/views.py
@@ -3,12 +3,14 @@ from project.recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
-    })
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(
+        request,
+        'recipes/pages/home.html',
+        context={
+            'recipes': recipes,
+        },
+    )
 
 
 def category(request, category_id):
@@ -16,21 +18,27 @@ def category(request, category_id):
         Recipe.objects.filter(
             category__id=category_id,
             is_published=True,
-        ).
-        order_by('-id')
+        ).order_by('-id')
     )
 
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
-        'title': f'{recipes[0].category.name} - Category | '
-    })
+    return render(
+        request,
+        'recipes/pages/category.html',
+        context={
+            'recipes': recipes,
+            'title': f'{recipes[0].category.name} - Category | ',
+        },
+    )
 
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe, pk=id, is_published=True)
 
-    return render(request, 'recipes/pages/recipe_view.html', context={
-        'recipe': recipe,
-        'is_detail_page': True,
-
-    })
+    return render(
+        request,
+        'recipes/pages/recipe_view.html',
+        context={
+            'recipe': recipe,
+            'is_detail_page': True,
+        },
+    )
