6ef5002249c3dd1bf94ef2938fa3651705671938
Vinicius Braga <Vbragadev@gmail.com>
2022-07-18 21:49:44 -0300

    add test recipe to template category

--- project/recipes/tests/test_recipe_views.py
+++ project/recipes/tests/test_recipe_views.py
@@ -54,3 +54,27 @@ class RecipeViewsTest(RecipeBase):
         self.assertEqual(
             response_get_not_found.status_code, HTTPStatus.NOT_FOUND
         )
+
+    def test_recipe_category_template_loads_recipes(self):
+        needed_title = 'This is a category test'
+        self.make_recipe(title=needed_title)
+
+        response_category = self.client.get(
+            reverse('recipes:category', args=(1,))
+        )
+        content_category = response_category.content.decode('utf-8')
+
+        self.assertIn(needed_title, content_category)
+
+    def test_recipe_template_loads_the_correct_recipe(self):
+        needed_title = 'This is a detail page - It load one recipe'
+
+        # Need a recipe for this test
+        self.make_recipe(title=needed_title)
+
+        response_category = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
+        content_category = response_category.content.decode('utf-8')
+
+        self.assertIn(needed_title, content_category)
