539be244022730d351fd19c11832bd2d0df65ba8
Vinicius Braga <Vbragadev@gmail.com>
2022-09-15 23:02:13 -0300

    validation error in forms

--- project/authors/forms.py
+++ project/authors/forms.py
@@ -1,5 +1,8 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import ValidationError
 
 
 def add_attr(field, attr_name, attr_new_value):
@@ -9,6 +12,19 @@ def add_attr(field, attr_name, attr_new_value):
 
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter,'
+            'one lowercase letter and one number. the length shoud be'
+            'at least 8 characters.'
+        ),
+            code='Invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
@@ -28,14 +44,15 @@ class RegisterForm(forms.ModelForm):
         error_messages={
             'required': 'Password must not be empty'
         },
-        help_text={
+        help_text=(
             'Password must have at least one uppercase letter,'
             'one lowercase letter and one number. the length shoud be'
             'at least 8 characters.'
-        }
+        ),
+        validators=[strong_password]
     )
 
-    password2 = forms.CharField(
+    repeat_password = forms.CharField(
         required=True,
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Repeat you password'
@@ -82,3 +99,14 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here'
             })
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        repeat_password = cleaned_data.get('repeat_password')
+
+        if password != repeat_password:
+            raise ValidationError({
+                'password': 'Password and repeat password must be equal',
+                'repeat_password': 'Password and repeat password must be equal'
+            })
